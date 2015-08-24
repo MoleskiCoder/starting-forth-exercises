95ce456d472df76ce9e6e9067094355d7c0fc793
Adrian Conlon <adrian.conlon@gmail.com>
2015-08-24 13:47:11 +0100

    Add solution to chapter 9, problem 1. (Note stack state documentation!)
    Signed-off-by: Adrian Conlon <adrian.conlon@gmail.com>

--- c9.fs
+++ c9.fs
@@ -13,3 +13,20 @@
 CR
 CONVICTED-OF ARSON HOMICIDE TAX-EVASION
 WILL-SERVE
+
+\ Problem 1, Multiply next word to give "counts"
+
+: COUNTS ( total n -- total )
+   ' ( total n vector )
+   ROT ( n vector total )
+   ROT ( vector total n )
+   ( vector total n ) 0 DO ( vector total )
+      ( vector total ) OVER ( vector total vector )
+      ( vector total vector ) EXECUTE ( vector total )
+   LOOP
+   SWAP ( total vector )
+   DROP ; ( total )
+
+CR
+CONVICTED-OF BOOKMAKING 3 COUNTS TAX-EVASION
+WILL-SERVE
